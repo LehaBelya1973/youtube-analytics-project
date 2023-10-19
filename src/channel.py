f5ff13577bd5f4b2ef8fcf7c7835894aae19341a
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2023-10-19 11:26:40 +0300

    Домашняя работа 1 - Ютюб-аналитика

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,9 @@
+import json
+
+import requests
+import os
+from googleapiclient.discovery import build
+from dotenv import load_dotenv
 
 
 class Channel:
@@ -5,8 +11,35 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        load_dotenv()
+        self.api_key = os.getenv('YT_API_KEY') # Получаем ключ из переменных окружения
+        self.channel = self.youtube().channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
 
-    def print_info(self) -> None:
+
+    def print_info(self) -> None | dict:
         """Выводит в консоль информацию о канале."""
-        pass
+        if not self.api_key:
+            print("Ключ API YouTube не задан. Установите его в переменных окружения YT_API_KEY.")
+            return
+
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @property
+    def title(self):
+        return self.print_info()['items'][0]['snippet']['title']
+
+    @property
+    def video_count(self):
+        return self.print_info()['items'][0]['statistics']['videoCount']
+
+    @property
+    def url(self):
+        return f"https://www.youtube.com/channel/{self.print_info()['items'][0]['id']}"
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    def youtube(self):
+        return build('youtube', 'v3', developerKey=self.api_key)
